35dd14da1dfe4ba01437e0dc35ef57b4c46010ab
Divit <56664482+DIMAX99@users.noreply.github.com>
2024-03-26 23:37:05 +0530

    Delete SportsConnect directory

--- SportsConnect/PHP/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-try{
-    $con=new PDO("mysql:host=localhost;dbname=sc","root","");
-}catch(PDOException $e){
-    echo $e->getMessage();
-}
--- SportsConnect/PHP/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include'db.php';
-
-session_start();
-session_unset();
-session_destroy();
-
-header('location:/SportsConnect/PHP/login.php');
